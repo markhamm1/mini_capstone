0484bba6cd1719107acee3dabb9419e22e12b5fc
Mark <markandrewhamm@gmail.com>
2020-06-30 20:08:39 -0500

    displayed Product first on separate page

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,10 +1,12 @@
 class Api::ProductsController < ApplicationController
 
   def product
+    @product = Product.first
     render "product.json.jb"
   end
 
   def products
+    @products = Product.all
     render "products.json.jb"
   end
 end
