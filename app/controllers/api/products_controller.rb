208cabd811f6de72772a23fb4e70cc67f699dec5
Mark <markandrewhamm@gmail.com>
2020-07-02 21:11:58 -0500

    show action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,4 +4,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'index.json.jb'
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
