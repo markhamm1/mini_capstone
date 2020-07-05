0dc9ef662e45d244cdaff925c84b59991c4c0398
Mark <markandrewhamm@gmail.com>
2020-07-05 10:21:24 -0500

    created update action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -20,4 +20,14 @@ class Api::ProductsController < ApplicationController
     @product.save
     render 'show.json.jb'
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render 'show.json.jb'    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     get "/products/:id" => "products#show"
 
     post "/products" => "products#create"
+
+    patch "/products/:id" => "products#update"
   end
 
 end
